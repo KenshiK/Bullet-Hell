f6baaa4b30c333d1162a35a417827aca7cfb6e37
Felscream <31777830+Felscream@users.noreply.github.com>
2018-06-02 19:33:56 -0400

    Started path following

---
+++ Assets/Scripts/AI/Path/PathWaypoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathWaypoint : MonoBehaviour {
+
+    [SerializeField] private List<Transform> waypoints;
+
+    void Awake()
+    {
+        if(waypoints.Count == 0)
+        {
+            Transform[] temp = GetComponentsInChildren<Transform>(false);
+            waypoints = new List<Transform>();
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] != transform)
+                {
+                    waypoints.Add(temp[i]);
+                }
+            }
+        }
+        
+    }
+    void OnDrawGizmos()
+    {
+        if(waypoints != null && waypoints.Count > 0)
+        {
+            Vector3 startPos = transform.GetChild(0).position;
+            Vector3 previousPos = startPos;
+            Gizmos.color = Color.yellow;
+            for (int i = 0; i < waypoints.Count; i++)
+            {
+                Gizmos.color = i == 0 ? Color.red : Color.yellow;
+                Gizmos.DrawSphere(waypoints[i].position, 0.3f);
+                previousPos = waypoints[i].position;
+            }
+        }
+        
+    }
+
+    public List<Transform> GetPath()
+    {
+        return waypoints;
+    }
+}
---
+++ Assets/Scripts/Editor/PathWaypointEditor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathWaypoint))]
+public class PathWaypointEditor : Editor {
+
+    void OnSceneGUI()
+    {
+        PathWaypoint path = target as PathWaypoint;
+        List<Transform> listTransform = path.GetPath();
+        if (listTransform == null || listTransform.Count == 0)
+            return;
+        // we store the start and end points of the line segments in this array
+        Vector3[] lineSegments = new Vector3[listTransform.Count * 2];
+
+        int lastObject = listTransform.Count - 1;
+        Vector3 prevPoint;
+        if (listTransform[lastObject])
+        {
+            prevPoint = listTransform[lastObject].position;
+        }
+        else
+        {
+            prevPoint = Vector3.zero;
+        }
+        int pointIndex = 0;
+        for (int currObjectIndex = 0; currObjectIndex < listTransform.Count; currObjectIndex++)
+        {
+            // find the position of our connected object and store it
+            Vector3 currPoint;
+            if (listTransform[currObjectIndex])
+            {
+                currPoint = listTransform[currObjectIndex].position;
+            }
+            else
+            {
+                currPoint = Vector3.zero;
+            }
+
+            // store the starting point of the line segment
+            lineSegments[pointIndex] = prevPoint;
+            pointIndex++;
+
+            // store the ending point of the line segment
+            lineSegments[pointIndex] = currPoint;
+            pointIndex++;
+
+            prevPoint = currPoint;
+        }
+        Handles.color = Color.yellow;
+        Handles.DrawLines(lineSegments);
+    }
+}
