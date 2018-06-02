91c56475d50c1315c0d9544f696cfac92009c9e7
Moïse <mbakim@hotmail.fr>
2018-06-01 20:42:21 -0400

    * Shitty finger follow * Camera is now othographic : - Bigger spaceship - A lot of size adjustement to come

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,33 +8,17 @@ public class PlayerController : MonoBehaviour
     public float tilt;
     public Boundary boundary;
 
+    public float xDiv, yDiv;
+
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        GetComponent<Rigidbody>().velocity = movement * speed;
-        
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        if (Input.touchCount > 0)
         {
-            // Get movement of the finger since last frame
-            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-
-            GetComponent<Rigidbody>().position = new Vector3(touchDeltaPosition.x, 0.0f, touchDeltaPosition.y);
-
-            // Move object across XY plane
-            //transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 5));
+            transform.position = Vector3.MoveTowards(transform.position, pos, xDiv / Time.deltaTime);
         }
 
-        GetComponent<Rigidbody>().position = new Vector3
-        (
-            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
-            0.0f,
-            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-        );
-
-        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+        //GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 
     }
 }
